f4ee74ed2a9d04bb902f50f06080cb331b3f2b0c
xpl <rocket.mind@gmail.com>
2018-04-05 06:35:34 +0300

    fix

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const { yellow } = require ('ansicolor')
 const { WebClient } = require ('@slack/client')
 const { accessToken } = require ('./config.json')
 
-if (accessToken) {
+if (accessToken !== '<YOUR ACCESS TOKEN HERE>') {
     log.bright.green ('Using access token:', yellow (accessToken))
+
 } else {
     log.bright.red ('No access token provided! Please check your config.json')
+    process.exit (1)
 }
 
